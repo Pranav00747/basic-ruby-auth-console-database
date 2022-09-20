b3a17a2f72e680bb87b8346aff929c83d895c321
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-09-21 01:39:46 +0530

    Update register_db.rb

--- register_db.rb
+++ register_db.rb
@@ -1,9 +1,9 @@
 require 'mongo'
 class Reg_db
-    def db_register(nm, eml, cmp, rl, st)
+    def db_register(nm, eml, status1)
         cl = Mongo::Client::new('mongodb://127.0.0.1:27017/basic_records')
         colnc =  cl[:registry]
-        r = colnc.insert_one({name:nm, email:eml, company:cmp, relation:rl, strenght:st})
+        r = colnc.insert_one({name:nm, email:eml, status:status1)
         if r==1
             puts "Sucessfully registered."
         else
